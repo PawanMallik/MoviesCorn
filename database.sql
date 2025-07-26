1743acd8d736a46af90db0be90cef1bba163eac2
Pawan <pawanmallick432003@gmail.com>
2025-07-26 19:02:43 +0530

    final

--- database.sql
+++ database.sql
@@ -1,27 +1,4 @@
--- CREATE DATABASE movie_requests;
-
--- CREATE TABLE users (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(50) NOT NULL UNIQUE,
---     email VARCHAR(100) NOT NULL UNIQUE,
---     password VARCHAR(255) NOT NULL,
---     time 
-    
--- );
-
-
--- CREATE TABLE movie_requests (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     full_name VARCHAR(100) NOT NULL,
---     email VARCHAR(100) NOT NULL,
---     movie_name VARCHAR(100) NOT NULL,
---     note TEXT
--- );
-
-
--- Create the database
--- CREATE DATABASE movie_requests;
--- USE movie_requests;
+-- Create database Movies_corn
 
 -- Create users table
 CREATE TABLE users (
